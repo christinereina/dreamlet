29a26699f6519779cbb1b41292afcfe9c5c7affd
blacchoy@gmail.com <blacchoy@gmail.com>
2020-09-18 11:34:49 -0700

    pass 2nd test

--- src/reducers/kombucha-list-reducer.js
+++ src/reducers/kombucha-list-reducer.js
@@ -1,3 +1,18 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, brand, price, flavor, quanity, id } = action;
+  switch (action.type) {
+  case 'ADD_KOMBUCHA':
+    return Object.assign({}, state, {
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        flavor: flavor,
+        quanity: quanity,
+        id: id
+      }
+    });
+  default:
+    return state;
+  }
 };
