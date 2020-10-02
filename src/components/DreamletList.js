d657292929ad58efe2fcd583b8cd8a73638e55ab
blacchoy@gmail.com <blacchoy@gmail.com>
2020-10-02 09:26:17 -0700

    add firestore to dreamletlist to display dreamlets

--- src/components/DreamletList.js
+++ src/components/DreamletList.js
@@ -4,32 +4,41 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Dreamlet from "./Dreamlet";
+import { useSelector } from 'react-redux'
+import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase'
 
 function DreamletList(props){
+  useFirestoreConnect([
+    { collection: 'dreamlets' }
+  ]);
 
-  return (
-  <React.Fragment> 
-    <Container>
-    <Row>
-      <Col xs={{ order: 'first' }}>
-          {Object.values(props.dreamletList).map((dreamlet) => {
-            return <Dreamlet
-              whenDreamletClicked = { props.onDreamletSelection }
-              title={dreamlet.title}
-              description={dreamlet.description}
-              id={dreamlet.id}
-              key={dreamlet.id}/>
-          })}
-        </Col>  
-      </Row>
-    </Container> 
-  </React.Fragment>
-  );
+  const dreamlets = useSelector(state => state.firestore.ordered.dreamlets);
+
+  if (isLoaded(dreamlets)) {
+    return (
+      <React.Fragment>
+        <hr/>
+        {dreamlets.map((dreamlet) => {
+          return <Dreamlet
+            whenDreamletClicked = { props.ondreamletSelection }
+            title={dreamlet.title}
+            description={dreamlet.description}
+            id={dreamlet.id}
+            key={dreamlet.id}/>
+        })}
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <h3>Loading...</h3>
+      </React.Fragment>
+    )
+  }
 }
 
- DreamletList.propTypes = {
-    dreamletList: PropTypes.object,
-    onDreamletSelection: PropTypes.func
-  };
+DreamletList.propTypes = {
+  ondreamletSelection: PropTypes.func
+};
 
 export default DreamletList;
