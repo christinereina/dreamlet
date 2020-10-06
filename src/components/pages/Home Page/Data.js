5aed2dd268d788f8e912ed0eea02785c64dec984
blacchoy@gmail.com <blacchoy@gmail.com>
2020-10-06 16:07:55 -0700

    add second home object

--- src/components/pages/Home Page/Data.js
+++ src/components/pages/Home Page/Data.js
@@ -1,7 +1,6 @@
 export const homeObjOne = {
   lightBg: false,
   primary: true,
-  lightBg: false,
   imgStart: '',
   lightTopLine: true,
   lightTextDesc: true,
@@ -10,7 +9,23 @@ export const homeObjOne = {
   headline: 'Ever wonder what others are dreaming about...',
   lightText: true,
   topLine: 'Dreamlet',
-  img: require('../../Images/homepage.png'),
+  img: require('../../Images/scene.png'),
   alt: 'Image',
   start: ''
+};
+
+export const homeObjTwo = {
+  lightBg: true,
+  primary: false,
+  imgStart: 'start',
+  lightTopLine: false,
+  lightTextDesc: false,
+  buttonLabel: 'Start with Dreamlet',
+  description: 'The goal of Dreamlet is to create community and connectedness through dreams.',
+  headline: 'Ever wonder what others are dreaming about...',
+  lightText: false,
+  topLine: 'Dreamlet',
+  img: require('../../Images/homepage.png'),
+  alt: 'Image',
+  start: 'true'
 };
